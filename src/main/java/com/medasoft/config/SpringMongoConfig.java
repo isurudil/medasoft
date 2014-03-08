860ede5ce35a156dfeaa0e74ba94e9ae463736ff
Isuru Dilshan <isurudilsh@gmail.com>
2014-03-08 18:43:40 +0530

    Yooo hoooo git statusgit status got mongo working at last :D :D

---
+++ src/main/java/com/medasoft/config/SpringMongoConfig.java
@@ -0,0 +1,28 @@
+package com.medasoft.config;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+/**
+ * Created by isurud on 3/8/14.
+ */
+public class SpringMongoConfig {
+
+    public @Bean
+    MongoDbFactory mongoDbFactory() throws Exception{
+
+       return  new SimpleMongoDbFactory(new MongoClient(),"meda");
+
+    }
+
+    public @Bean
+    MongoTemplate mongoTemplate() throws Exception{
+
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+        return  mongoTemplate;
+
+    }
+}
--- src/main/java/com/medasoft/model/dto/User.java
+++ src/main/java/com/medasoft/model/dto/User.java
@@ -1,24 +1,42 @@
 package com.medasoft.model.dto;
 
+
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.List;
-
 /**
- * Created by isurud on 3/5/14.
+ * Created by isurud on 3/8/14.
  */
-@Document(collection = "user")
+@Document(collection = "doctor_info")
 public class User {
 
     @Id
-    private String id;
-    private String username;
-    private String password;
-    private List roles;
+    private String _id;
+    String username;
+    String password;
 
+    public String getUsername() {
+        return username;
+    }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
+    public String getPassword() {
+        return password;
+    }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public String get_id() {
+        return _id;
+
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
 }
