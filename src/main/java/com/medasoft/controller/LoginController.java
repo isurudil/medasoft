31677e1cf392dd1249b518fb2c49e83ca32a0bcc
Isuru Dilshan <isurudilsh@gmail.com>
2014-04-23 23:42:33 +0530

    Adding RestClient class functionality

--- src/main/java/com/medasoft/controller/LoginController.java
+++ src/main/java/com/medasoft/controller/LoginController.java
@@ -3,21 +3,19 @@ package com.medasoft.controller;
 import com.google.gson.Gson;
 import com.medasoft.model.response.UserLoginResponse;
 import com.medasoft.util.Configuration;
+import com.medasoft.util.RestClient;
 import com.medasoft.view.LoginViewComponent;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
-import org.apache.commons.httpclient.Header;
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashMap;
 
 @Controller
 public class LoginController {
@@ -55,26 +53,14 @@ public class LoginController {
     public void authUser() {
         Gson gson = new Gson();
         UserLoginResponse userLoginResponse;
+        HashMap parameterHashMap = new HashMap();
 
         String url = "http://localhost:8080/meda/rest/user/get/";
         try {
             url = url + URLEncoder.encode(txtUserText, "UTF-8");
-            HttpClient client = new HttpClient();
-            PostMethod mPost = new PostMethod(url);
-            mPost.addParameter("username", txtUserText);
-
-            Header mtHeader = new Header();
-            mtHeader.setName("content-type");
-            mtHeader.setValue("application/x-www-form-urlencoded");
-            mtHeader.setName("accept");
-            mtHeader.setValue("application/text");
-            mPost.addRequestHeader(mtHeader);
-            client.executeMethod(mPost);
-            String output = mPost.getResponseBodyAsString();
-            LOGGER.debug("Received Response : " + output);
+            parameterHashMap.put("username", txtUserText);
+            String output = new RestClient().post(url,parameterHashMap);
             userLoginResponse = gson.fromJson(output, UserLoginResponse.class);
-            LOGGER.debug("Query Success .... got password " + output + " for username " + txtUserText);
-            mPost.releaseConnection();
             if (userLoginResponse.getStatus().equals(Configuration.SUCCESS_CODE_DEFAULT)) {
 
                 if (userLoginResponse.getPassword().equals(txtPassText)) {
@@ -87,8 +73,6 @@ public class LoginController {
 
             }
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (HttpException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/medasoft/util/RestClient.java
+++ src/main/java/com/medasoft/util/RestClient.java
@@ -1,7 +1,46 @@
 package com.medasoft.util;
 
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 /**
  * Created by isurud on 4/23/14.
  */
 public class RestClient {
+
+    private static final Logger LOGGER = Logger.getLogger(RestClient.class);
+
+    public String post(String url,HashMap parameterMap){
+
+        String output = null;
+        HttpClient client = new HttpClient();
+        PostMethod mPost = new PostMethod(url);
+        Iterator iterator = parameterMap.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry entry = (Map.Entry) iterator.next();
+            mPost.addParameter(entry.getKey().toString(),entry.getValue().toString());
+        }
+        Header mtHeader = new Header();
+        mtHeader.setName("content-type");
+        mtHeader.setValue("application/x-www-form-urlencoded");
+        mtHeader.setName("accept");
+        mtHeader.setValue("application/text");
+        mPost.addRequestHeader(mtHeader);
+        try {
+            client.executeMethod(mPost);
+            output = mPost.getResponseBodyAsString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LOGGER.debug("Received Response : " + output);
+        mPost.releaseConnection();
+        return output;
+    }
 }
